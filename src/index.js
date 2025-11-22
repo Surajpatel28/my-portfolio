a5c0bec2a796ead0f01b538fd78f497ddb3ae617
Suraj <suraj123patel123@gmail.com>
2025-11-22 18:06:16 +0530

    removed the font imports

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/600.css';
-import '@fontsource/playfair-display/400.css';
-import '@fontsource/playfair-display/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
